3a46017585ba65e8ee03ce79b1f581aaad770674
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-04-21 18:19:55 +0200

    updt projectseeder created tech -msrc

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator as Faker;
 class ProjectSeeder extends Seeder
 {
@@ -13,11 +14,15 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
+      $types = Type::all()->pluck('id');
+
       for ($i=0; $i < 30; $i++) { 
         $new_project =  new Project;
 
         $new_project->title = $faker->catchPhrase();
         $new_project->description = $faker->paragraph();
+        $new_project->type_id = $faker->randomElement($types);
         // $new_project->link = $faker->imageUrl(640, 480, 'project', true);
         $new_project->save();
       }
